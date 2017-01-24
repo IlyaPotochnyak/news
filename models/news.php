9247653c4b6b276c3557553a1d02319d8dd684fc
Илья Поточняк < >
2017-01-24 16:44:36 +0200

    Revert "new commit"
    This reverts commit 9cea7eed512d6367ed7e5d823df9274b90b8a213.

---
+++ models/news.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once __DIR__ . '/../functions/sql.php';
+function news_getAll()
+{
+  sqlConnect();
+
+  $sql = 'SELECT * FROM news';
+
+  return sql_query($sql);
+
+}
+
+function news_insert($data)
+{
+  // $sql = "ISERT INTO news
+  //         (title, text)
+  //         VALUES
+  //         ('" . $data['title'] ."', '" . $data['title'] ."')
+  // ";
+  $date = date("d.m.Y H : i : s");
+  $u_date = date("U");
+  $sql = "INSERT INTO news (date, u_date, title, text)
+  VALUES ('" . $date ."', '" . $u_date ."', '" . $data['title'] ."', '" . $data['text'] ."')";
+  // echo $sql;
+
+  sql_exec($sql);
+}
+
+
+ ?>
